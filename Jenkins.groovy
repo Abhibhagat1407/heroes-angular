016127820e4d0459ba990a0d9ca95e0edcaf2fe6
Abhibhagat1407 <109520000+Abhibhagat1407@users.noreply.github.com>
2024-03-21 01:46:20 +0530

    Create Jenkins.groovy

---
+++ Jenkins.groovy
@@ -0,0 +1,49 @@
+pipeline {
+    agent any
+    
+    stages {
+        stage('Clearing Workspace') {
+            steps {
+                cleanWs()
+            }
+        }
+        
+        stage('Cloning the Repository') {
+            steps {
+                sh 'git clone "https://github.com/Abhibhagat1407/heroes-angular.git"'
+            }
+        }
+        
+        stage('Changing to Repository Directory') {
+            steps {
+                dir('heroes-angular') {
+                    // Run commands inside the 'heroes-angular' directory
+                    sh 'npm install'
+                    
+                    // Copy Nginx configuration
+                    sh 'sudo cp heroes-angular.conf /etc/nginx/sites-available/'
+                    
+                    // Enable site
+                    sh 'sudo rm -f /etc/nginx/sites-enabled/heroes-angular.conf'
+                    sh 'sudo ln -s /etc/nginx/sites-available/heroes-angular.conf /etc/nginx/sites-enabled/'
+                    
+                    // Reload Nginx
+                    sh 'sudo systemctl reload nginx'
+                    
+                    // Start application
+                    sh 'npm run quick'
+                    sh 'npm start'
+                }
+            }
+        }
+        
+        // Add other stages as needed
+    }
+    
+    post {
+        failure {
+            echo 'Pipeline failed'
+            // Add any failure handling steps here
+        }
+    }
+}
